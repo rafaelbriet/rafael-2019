3098ce7debe195b373e3087f3780a86169e31d68
Rafael Briet <rafael-briet@hotmail.com>
2019-02-06 13:05:20 -0200

    Página inicial responsiva e links para redes sociais

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 
 <footer class="site-footer">
     <nav class="social-links">
-        <a href="https://rafaelbriet.com.br" target="_blank" class="social-icons icon-Facebook_white"></a>
-        <a href="https://rafaelbriet.com.br" target="_blank" class="social-icons icon-LinkedIN_white"></a>
-        <a href="https://rafaelbriet.com.br" target="_blank" class="social-icons icon-Instagram_white"></a>
-        <a href="https://rafaelbriet.com.br" target="_blank" class="social-icons icon-Behance_white"></a>
+        <a href="https://www.facebook.com/rafaelbriet/" target="_blank" class="social-icons icon-Facebook_white"></a>
+        <a href="https://www.instagram.com/rafael_briet/" target="_blank" class="social-icons icon-Instagram_white"></a>
+        <a href="https://www.linkedin.com/in/rafaelbriet/" target="_blank" class="social-icons icon-LinkedIN_white"></a>        
+        <a href="https://www.behance.net/rafaelbriet" target="_blank" class="social-icons icon-Behance_white"></a>
     </nav>
 </footer><!-- .site-footer -->
 </div><!-- .container -->
