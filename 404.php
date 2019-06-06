205604318fb307d0fc0a1f1ab381298a304aa877
rafaelbriet <rafael-briet@hotmail.com>
2019-06-06 16:28:05 -0300

    Criei a página para lidar caso alguém tente acessar uma página que não exita (Erro 404)

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="center">
+
+    <h1>Ops! A Página não foi encontrada.</h1>
+
+    <p>Infelimente a página que você está tentando acessar não está aqui. Se você tem certeza que você o endereço é esse mesmo, talvez a página tenha sido renomeada, movida ou apagada.</p>
+    <p>Você pode voltar a <a href="<?php echo get_home_url(); ?>">página inicial</a> ou usar o campo de pesquisa abaixo:</p>
+
+    <?php get_search_form(); ?>
+
+</div>
+
+<?php get_footer(); ?>
