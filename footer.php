1328f4719569e04d7871a2dfc55b307c5be586ee
Rafael Briet <rafael-briet@hotmail.com>
2020-05-14 20:51:01 -0300

    Removi os icones do IcoMoon e adicionei os icones do Font Awesome

--- footer.php
+++ footer.php
@@ -3,14 +3,18 @@
 <footer class="site-footer">
     <div class="site-footer-center">
     <nav class="social-links">
-        <a href="https://www.facebook.com/rafaelbriet/" target="_blank" class="social-icons icon-Facebook_white"></a>
-        <a href="https://www.instagram.com/rafael_briet/" target="_blank" class="social-icons icon-Instagram_white"></a>
-        <a href="https://www.linkedin.com/in/rafaelbriet/" target="_blank" class="social-icons icon-LinkedIN_white"></a>        
-        <a href="https://www.behance.net/rafaelbriet" target="_blank" class="social-icons icon-Behance_white"></a>
+
+        <a href="https://www.facebook.com/rafaelbriet/" target="_blank" class="social-icons"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/rafael_briet/" target="_blank" class="social-icons"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/rafaelbriet/" target="_blank" class="social-icons"><i class="fab fa-linkedin-in"></i></a>        
+        <a href="https://www.behance.net/rafaelbriet" target="_blank" class="social-icons"><i class="fab fa-behance"></i></a>
+
     </nav>
     </div><!-- .site-footer-center -->
 </footer><!-- .site-footer -->
 
+<script src="https://kit.fontawesome.com/5a3e06cedb.js" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
+
 </body>
 </html>
