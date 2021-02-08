8c2f23e9335040d6d73cb4d7688913d14b2ba7ce
Rafael Briet <rafael-briet@hotmail.com>
2021-02-08 11:37:18 -0300

    Adicionei HTML doctype

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
