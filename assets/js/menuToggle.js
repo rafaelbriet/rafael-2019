dcc7f639c3e97acdf5eb97def18af0a9f2943b98
Rafael Briet <rafael-briet@hotmail.com>
2021-02-12 18:31:53 -0300

    Para de usar jQuery nos menus

--- assets/js/menuToggle.js
+++ assets/js/menuToggle.js
@@ -1,14 +1,23 @@
-jQuery(document).ready(function() {
+document.addEventListener('DOMContentLoaded', () => {
+  const menuHeaderToggle = document.querySelector('.menu-header-toggle');
+  const blogNavToggle = document.querySelector('.blog-nav-toggle');
   
-  // Toggle open the primary navigation
-  jQuery('.menu-header-toggle').click(function() {
-    jQuery('.menu-header').toggleClass('menu-header-open');
-    jQuery('.main-nav').toggleClass('menu-open');
-    jQuery('body').toggleClass('no-scroll');
-  });
-
-  // Toggle open the categories menu
-  jQuery('.blog-nav-toggle').click(function() {
-    jQuery('.blog-categories-list').toggleClass('blog-nav-open');
-  });
+  menuHeaderToggle.addEventListener('click', () => togglePrimaryMenu());
+  blogNavToggle.addEventListener('click', () => toggleCategoryMenu());
 });
+
+function togglePrimaryMenu() {
+  const menuHeader = document.querySelector('.menu-header');
+  const maiNav = document.querySelector('.main-nav');
+  const body = document.querySelector('body');
+  
+  menuHeader.classList.toggle('menu-header-open');
+  maiNav.classList.toggle('menu-open');
+  body.classList.toggle('no-scroll');
+}
+
+function toggleCategoryMenu() {
+  const blogCategoriesList = document.querySelector('.blog-categories-list');
+  
+  blogCategoriesList.classList.toggle('blog-nav-open');
+}
--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ add_action( 'after_setup_theme', 'rafaelbriet_content_width', 0 );
 function rafaelbriet_scripts() {
 	wp_enqueue_style( 'rafaelbriet-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 
-	wp_enqueue_script( 'rafaelbriet-menutoogle', get_template_directory_uri() . '/assets/js/menuToggle.js', array('jquery'), null, true);
+	wp_enqueue_script( 'rafaelbriet-menutoogle', get_template_directory_uri() . '/assets/js/menuToggle.js', null, null, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
